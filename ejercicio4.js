5df18603de6eef724e06c5f4792ca8fbc90c7bed
Herbenishd87 <herbenis.hernandez632@asys.edu.co>
2024-09-19 21:15:46 -0500

    agregar el ejercicio4

---
+++ ejercicio4.js
@@ -0,0 +1,9 @@
+const nombre = prompt("¿ cual es tu nombre?")
+//const edad = parseInt(prompt("¿ cual es tu edad?")
+
+const edad2 = parseInt(prompt("¿ cual es tu edad?"))
+
+console.log(nombre)
+console.log(typeof nombre)
+console.log(edad2)
+console.log(typeof edad2)
